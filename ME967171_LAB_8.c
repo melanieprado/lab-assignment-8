b40da6a265204883afe2be429f8f7afcea6be613
melanieprado <134087719+melanieprado@users.noreply.github.com>
2023-07-14 17:02:47 -0400

    Update ME967171_LAB_8.c

--- ME967171_LAB_8.c
+++ ME967171_LAB_8.c
@@ -6,7 +6,6 @@ int extraMemoryAllocated;
 
 void swap(int* a, int* b)
 {
-
     int temp = *a;
     *a = *b;
     *b = temp;
@@ -19,36 +18,22 @@ void heapify(int arr[], int N, int i)
 {
     // Find largest among root,
     // left child and right child
-
-    // Initialize largest as root
     int largest = i;
-
-    // left = 2*i + 1
-    int left = 2 * i + 1;
-
-    // right = 2*i + 2
-    int right = 2 * i + 2;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
 
     // If left child is larger than root
-    if (left < N && arr[left] > arr[largest])
-
+    if (left<N && arr[left]>arr[largest]){
         largest = left;
-
+    }
     // If right child is larger than largest
-    // so far
-    if (right < N && arr[right] > arr[largest])
-
+    if (right<N && arr[right]>arr[largest]){
         largest = right;
-
+    }
     // Swap and continue heapifying
-    // if root is not largest
-    // If largest is not root
-    if (largest != i) {
-
+    if (largest != i){
         swap(&arr[i], &arr[largest]);
-
-        // Recursively heapify the affected
-        // sub-tree
+        // Recursively heapify
         heapify(arr, N, largest);
     }
 }
@@ -57,18 +42,13 @@ void heapify(int arr[], int N, int i)
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n) {
       // Build max heap
-    for (int i = n / 2 - 1; i >= 0; i--)
-
+    for (int i=n/2-1; i>=0; i--){
         heapify(arr, n, i);
-
+    }
     // Heap sort
-    for (int i = n - 1; i >= 0; i--) {
-
+    for (int i=n-1; i>=0; i--) {
         swap(&arr[0], &arr[i]);
-
         // Heapify root element
-        // to get highest element at
-        // root again
         heapify(arr, i, 0);
     }
 }
